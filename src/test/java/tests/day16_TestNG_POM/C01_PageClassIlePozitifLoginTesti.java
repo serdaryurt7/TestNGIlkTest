4715d33778d0f654734fbe282dbe51bbb6ab4fb3
Serdar Yurtseven <serdaryurt7@gmail.com>
2024-10-05 15:05:59 +0300

    16.gun sonu

--- src/test/java/tests/day16_TestNG_POM/C01_PageClassIlePozitifLoginTesti.java
+++ src/test/java/tests/day16_TestNG_POM/C01_PageClassIlePozitifLoginTesti.java
@@ -1,8 +1,10 @@
 package tests.day16_TestNG_POM;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.TestOtomasyonPage;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class C01_PageClassIlePozitifLoginTesti {
 
@@ -20,9 +22,19 @@ public class C01_PageClassIlePozitifLoginTesti {
         testOtomasyonPage.accountLinki.click();
 
         //3- Kullanıcı email'i olarak "wise@gmail.com" girin
+        testOtomasyonPage.emailKutusu.sendKeys("wise@gmail.com");
+
         //4- Kullanıcı şifresi olarak "12345" girin
+        testOtomasyonPage.passwordKutusu.sendKeys("12345");
+
         //5- Login butonuna basarak login olun
+        testOtomasyonPage.loginButonu.click();
+
         //6- Başarılı olarak giriş yapılabildiğini test edin
+        Assert.assertTrue(testOtomasyonPage.logoutButonu.isDisplayed());
+        ReusableMethods.bekle(5);
+        Driver.quitDriver();
+
 
     }
 
---
+++ src/test/java/tests/day16_TestNG_POM/C02_PageClassKullanimi.java
@@ -0,0 +1,39 @@
+package tests.day16_TestNG_POM;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestOtomasyonPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C02_PageClassKullanimi {
+
+    @Test
+    public void aramaTesti() {
+        //1- testotomasyonu anasayfaya gidin
+        Driver.getDriver().get("https://www.testotomasyonu.com/");
+        //2- phone için arama yapın
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+        testOtomasyonPage.aramaKutusu.sendKeys("phone" + Keys.ENTER);
+        //3- Arama sonucunda bulunan ürün sayısının 3'den çok olduğunu test edin
+        int actualSonucSayisi = testOtomasyonPage.bulunanUrunElementleriList.size();
+        int expectedMinSonucSayisi = 3;
+
+        Assert.assertTrue(actualSonucSayisi > expectedMinSonucSayisi);
+
+        //4- ilk ürünü tıklayın
+        testOtomasyonPage.bulunanUrunElementleriList.get(0).click();
+
+        //5- açılan ürün sayfasında, ürün isminde case sensitive olmadan phone bulunduğunu test edin.
+        String expectedUrunIsimIcerigi = "phone";
+        String actualUrunIsmiKucukHarf = testOtomasyonPage.urunSayfasindaUrunIsimElementi.getText().toLowerCase();
+
+        Assert.assertTrue(actualUrunIsmiKucukHarf.contains(expectedUrunIsimIcerigi));
+
+        ReusableMethods.bekle(3);
+        Driver.quitDriver();
+
+    }
+
+}
