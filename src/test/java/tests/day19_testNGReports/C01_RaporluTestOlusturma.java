1ec2a4192dbdde418082f3f10c91e5d4f57209ea
Serdar Yurtseven <serdar.yurtseven@andasis.com>
2024-10-17 09:30:00 +0300

    19.gun sonu

--- src/test/java/tests/day19_testNGReports/C01_RaporluTestOlusturma.java
+++ src/test/java/tests/day19_testNGReports/C01_RaporluTestOlusturma.java
@@ -13,27 +13,40 @@ import utilities.TestBaseRapor;
 public class C01_RaporluTestOlusturma extends TestBaseRapor {
 
     @Test
-    public void aramaTesti(){
+    public void aramaTesti() {
+
+        /*
+            Raporlu olmasını istediğimiz her test method'unda
+            1- class'in TestbaseRapor'a child yapılması
+            2- extentTest objesinin oluşturulması
+            3- raporda cikmasini istedigimiz test adimlarini
+               extentTest objesi ile işlemeliyiz
+         */
+
+        extentTest = extentReports.createTest("arama testi", "test otomasyonunda phone aranabilmeli");
 
         // test otomasyonu ana sayfaya gidin
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        extentTest.info("kullanici test otomasyou ana sayfaya gider");
 
         // testotomasyonu sayfasına gittiğinizi test edin
-        String expectedUrl = ConfigReader.getProperty("toUrl") + "/asdf";
+        String expectedUrl = ConfigReader.getProperty("toUrl") + "/";
         String actualUrl = Driver.getDriver().getCurrentUrl();
 
         Assert.assertEquals(actualUrl, expectedUrl);
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(actualUrl, expectedUrl, "URL testi failed"); // 1.assertion
-
+        extentTest.info("testotomasyonu sayfasina gittigini test eder");
 
         // phone için arama yapın
         TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
         testOtomasyonPage.aramaKutusu.sendKeys("phone" + Keys.ENTER);
+        extentTest.info("phone icin arama yapar");
 
         // bulunan ürün sayısının 3'den fazla olduğunu test edin
-        int expectedMinUrunSayisi = 33;
+        int expectedMinUrunSayisi = 3;
         int actualUrunSayisi = testOtomasyonPage.bulunanUrunElementleriList.size();
+        extentTest.info("bulunan urun sayısının 3'den fazla olduğunu test eder");
 
         // Assert.assertTrue(actualUrunSayisi > expectedMinUrunSayisi);
         softAssert.assertTrue(actualUrunSayisi > expectedMinUrunSayisi, "Urun sayisi testi failed"); // 2.assertion
@@ -41,21 +54,24 @@ public class C01_RaporluTestOlusturma extends TestBaseRapor {
         // ilk ürüne tıklayın
         ReusableMethods.bekle(2);
         testOtomasyonPage.bulunanUrunElementleriList.get(0).click();
+        extentTest.info("ilk urune tiklar");
 
         // açılan ürün sayfasında, ürün isminin
         // case sensitive olmadan phone içerdiğini test edin
 
-        String expectedUrunIsimIcerik = "phoneDDD";
+        String expectedUrunIsimIcerik = "phone";
         String actualUrunIsmiKucukHarf = testOtomasyonPage.urunSayfasindaUrunIsimElementi.getText().toLowerCase();
+        extentTest.info("urun isminin case sensitive olmadan phone icerdigini test eder");
 
         // Assert.assertTrue(actualUrunIsmiKucukHarf.contains(expectedUrunIsimIcerik));
         softAssert.assertTrue(actualUrunIsmiKucukHarf.contains(expectedUrunIsimIcerik), "Urun ismi testi failed");
-
+        ReusableMethods.bekle(2);
         softAssert.assertAll();
-
+        extentTest.pass("softassert ile yapilan tum assertionlar raporlanir");
         // sayfayı kapatın
 
         Driver.quitDriver();
+        extentTest.info("sayfayi kapatir");
 
     }
 
---
+++ src/test/java/tests/day19_testNGReports/C02_RaporluAramaSepetiTesti.java
@@ -0,0 +1,54 @@
+package tests.day19_testNGReports;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestOtomasyonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
+
+public class C02_RaporluAramaSepetiTesti extends TestBaseRapor {
+
+    @Test
+    public void aramaSepetiTesti() {
+
+        extentTest = extentReports.createTest("arama sepeti testi", "sepete attigimiz urun ismi ile sepetteki isim ayni olmali");
+
+
+        //1- https://www.testotomasyonu.com/ anasayfasına gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        extentTest.info("Kullanici testotomasyonu anasayfaya gider");
+
+        //2- phone için arama yapın
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+        testOtomasyonPage.aramaKutusu.sendKeys("phone" + Keys.ENTER);
+        extentTest.info("phone icin arama yapar");
+
+        //3- Listelenen sonuçlardan ilkini tıklayın
+        testOtomasyonPage.bulunanUrunElementleriList.get(0).click();
+        extentTest.info("Listelenen sonuclardan ilkini tiklar");
+
+        //4- ürün ismini kaydedin ve sepete ekleyin
+        String sayfadakiUrunIsmi = testOtomasyonPage.urunSayfasindaUrunIsimElementi.getText();
+        testOtomasyonPage.addToCartButonu.click();
+        extentTest.info("urun ismini kaydeder ve sepete ekler");
+
+        //5- your cart linkine tıklayın
+        testOtomasyonPage.yourCart.click();
+        extentTest.info("your cart linkine tiklar");
+
+        //6- kaydettiğiniz ürün ismi ile sepetteki ürün isminin aynı olduğunu test edin
+        String sepettekiUrunIsmi = testOtomasyonPage.sepettekiUrunIsimElementi.getText();
+        ReusableMethods.bekle(1);
+        Assert.assertEquals(sepettekiUrunIsmi.toLowerCase(), sayfadakiUrunIsmi.toLowerCase());
+        extentTest.pass("kaydettigi urun ismi ile sepetteki urun isminin ayni oldugunu test eder");
+
+        //7- sayfayı kapatın
+        Driver.quitDriver();
+        extentTest.info("sayfayi kapatir");
+
+    }
+
+}
